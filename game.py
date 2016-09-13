842d85687d8110c0c470ee538e4ce1f209e7acf7
Josh Wilson <joshpwilson55@gmail.com>
2016-09-13 15:59:14 +1200

    Clearing
    probably still doesn't work on some platforms

--- game.py
+++ game.py
@@ -72,10 +72,13 @@ class Game:
 
     @staticmethod
     def clear_screen():
+
+        print("\033c", end="")
         if os.name == 'nt':
             os.system('cls')
             print("\033c", end="")
         else:
+            os.system("clear")
             print("\033c", end="")
 
     @staticmethod
